81a739da916bda770788ab5c9c1aadf2005170e6
Alessandro Marega <ale_marega@hotmail.it>
2021-07-21 18:40:49 +0200

    fix layout pagine status e dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,7 +14,7 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
 
-            <table class=" table ">
+            <table class=" table table-striped table-hover thead-dark">
                 <thead>
                   <tr>
                     <th scope="col">Id</th>
@@ -22,7 +22,7 @@
                     <th scope="col">Settore</th>
                     <th scope="col">Descrizione</th>
                     <th scope="col">Data e ora</th>
-                    <th scope="col">Stato</th>
+                    <th class="text-center" scope="col">Stato</th>
                     <th scope="col">Azioni</th>
                     <th scope="col">Steps</th>
                   </tr>
@@ -34,7 +34,7 @@
                         <td>
                             <p>{{$work->id}}</p>
                         </td>
-                        <td>
+                        <td class="font-weight-bold">
                             <p>{{$work->name}}</p>
                         </td>
                         <td>
@@ -46,24 +46,24 @@
                         <td>
                             <p><span>{{$work->created_at->format('d/m/Y H:i')}}</span> </p>
                         </td>
-                        <td>
+                        <td class="text-center">
                             @if ($work->status == 'created')
                                 <a href="{{ route('status', ['id' => $work->id]) }}">
-                                    <p>
+                                    <p class="d-inline">
                                         <i class="fas fa-arrow-circle-down"></i>
                                     </p>
                                 </a>
                             @endif
                             @if ($work->status == 'on_work')
                                 <a href="{{ route('status', ['id' => $work->id]) }}">
-                                    <p>
+                                    <p class="d-inline">
                                         <i class="fas fa-hammer"></i>
                                     </p>
                                 </a>
                             @endif
                             @if ($work->status == 'completed')
                                 <a href="{{ route('status', ['id' => $work->id]) }}">
-                                    <p>
+                                    <p class="d-inline">
                                         <i class="fas fa-check-circle"></i>
                                     </p>
                                 </a>
--- resources/views/status.blade.php
+++ resources/views/status.blade.php
@@ -10,34 +10,52 @@
 @endsection
 
 @section('content')
-    {{-- @foreach ($status as $stat)
-        <p>{{$stat}}</p>
-    @endforeach --}}
-    <div class="container">
+   
+    <div class="status container d-flex flex-column align-items-center">
+
+      {{-- bottone link a dashboard --}}
+      <div>
+        <a href="{{route('dashboard')}}">
+          <button class="btn btn-dark"">Torna alla Dashboard senza salvare</button>
+        </a>
+      </div>
+      {{-- /bottone link a dashboard --}}
+
+
+      
       @foreach ($myWork as $work)
-        <form action="{{route('status.update', ['id' => $work->id])}}" method="POST"  class="">
+        {{-- titolo --}}
+        <h1 class="mt-5">Modifica lo stato di '{{$work->name}}'</h1>
+        {{-- /titolo --}}
+
+        {{-- form --}}
+        <form action="{{route('status.update', ['id' => $work->id])}}" method="POST"  class="my-5">
           @method('PATCH')
           @csrf
             <div>
+              <label class="lab_name" for="created">Creato</label>
               <input type="radio" name="status" id="created" value="created" {{$work->status == 'created'? 'checked' : null}}>
               <label for="created"><i class="fas fa-arrow-circle-down"></i></label>
             </div>
 
             <div>
+              <label class="lab_name" for="on_work">In lavoro</label>
               <input type="radio" name="status" id="on_work" value="on_work" {{$work->status == 'on_work'? 'checked' : null}}>
               <label for="on_work"><i class="fas fa-hammer"></i></label>
             </div>
 
             <div>
+              <label class="lab_name" for="completed">Completato</label>
               <input type="radio" name="status" id="completed" value="completed" {{$work->status == 'completed'? 'checked' : null}}>
               <label for="completed"><i class="fas fa-check-circle"></i></label>
             </div>
 
-            <div class="form-fields">
+            <div class="form-fields text-center mt-3">
               <button type="submit" class="btn btn-success mt-3">Salva!</button>
             </div>
         </form>
-          
+        {{-- /form --}}          
       @endforeach
+
     </div>
 @endsection
